711a72383be46ff8d6014da8d3591342488aa23c
Aid Puska <aidpuska29@gmail.com>
2023-02-18 00:53:40 +0100

    Added animations

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -5,8 +5,8 @@ const Bar = ({ movie, handleClick, liked, fav, trashFull }) => {
     const img = 'https://image.tmdb.org/t/p/w500';
 
     return (
-        <div key={movie.id} className='relative shadow shadow-black h-fit w-60 font-light gap-5'>
-            <img className='h-60 w-60' src={`${img}${movie.poster_path}`} alt="" />
+        <div key={movie.id} className='relative shadow shadow-black h-fit w-60 font-light gap-5 hover:scale-110 transition ease-in-out duration-300'>
+            <img className='h-60 w-60 shadow shadow-black' src={`${img}${movie.poster_path}`} alt="" />
             <img onClick={() => handleClick(movie.id, movie.title, movie.poster_path)} src={liked.findIndex(item => item.id === movie.id) === -1 ? fav : trashFull} alt="" />
             <p className='text-medium font-semibold ml-1'>{movie.title || movie.name}</p>
             <p className='ml-1'>{movie.release_date || movie.first_air_date}</p>
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -8,15 +8,18 @@ import clock from '../assets/clock.svg'
 const Random = () => {
 
     const [tvShow, setTvShow] = useState(null)
+    const [loading, setLoading] = useState(false)
 
     const key = "4f80e483806e8be2daa79620bca00792"
     let id = Math.floor(Math.random() * 1000) + 1;
 
     useEffect(() => {
         const fetch = async () => {
+            setLoading(true);
             const res = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`)
             setTvShow(res.data)
-            //console.log(res.data)
+            setLoading(false);
+            console.log('res: ', res.data)
         }
 
         fetch();
@@ -24,15 +27,18 @@ const Random = () => {
 
     const handleClick = async () => {
         id = Math.floor(Math.random() * 1000) + 1;
+        setLoading(true)
         const res = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`)
         setTvShow(res.data)
+        setLoading(false)
+        console.log('res: ', res)
     }
 
     return (
         <div className='h-fit w-full mt-5 bg-gradient-to-t from-sky-900'>
-            {tvShow &&
+            {tvShow && !loading ?
                 <div className='pl-10 mb-10 flex items-center gap-5'>
-                    <img className='h-[500px] w-[700px] rounded shadow shadow-black' src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`} alt="" />
+                    <img className='h-[500px] w-[700px] rounded shadow shadow-black hover:translate-x-6 transition ease-in-out duration-150' src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`} alt="" />
                     <div className='mx-auto self-center space-y-5'>
                         <h3 className='text-center text-xl font-semibold'>{tvShow.title}</h3>
                         <p className='text-center font-light'>{tvShow.release_date}</p>
@@ -48,8 +54,26 @@ const Random = () => {
                             <p>{tvShow.runtime} minutes</p>
                         </div>
                     </div>
-                    <img onClick={handleClick} className='w-7 h-7 self-center mx-auto cursor-pointer' src={right} alt="" />
-                </div>}
+                    <img onClick={handleClick} className='w-7 h-7 self-center mx-auto cursor-pointer hover:scale-125' src={right} alt="" />
+                </div>
+                :
+                <div className='pl-10 mb-10 flex items-center gap-5'>
+                    <p className='h-[500px] w-[700px] rounded shadow shadow-black animate-pulse bg-gradient-to-t from-sky-900 flex items-center justify-center'>
+                        <p className='h-[450px] w-[650px] rounded shadow shadow-black animate-pulse bg-rose-100'></p>
+                    </p>
+                    <div className='mx-auto self-center space-y-5 animate-pulse'>
+                        <p className='w-80 h-80 shadow-black rounded bg-gradient-to-t from-sky-900 flex flex-col gap-5 items-center justify-center'>
+                            <div className='w-72 h-7 mb-5 rounded-md bg-rose-100 animate-pulse'></div>
+                            <div className='w-72 h-5 rounded-md bg-rose-100 animate-pulse'></div>
+                            <div className='w-72 h-5 rounded-md bg-rose-100 animate-pulse'></div>
+                            <div className='w-72 h-5 rounded-md bg-rose-100 animate-pulse'></div>
+                            <div className='w-72 h-5 rounded-md bg-rose-100 animate-pulse'></div>
+                            <div className='w-72 h-5 rounded-md bg-rose-100 animate-pulse'></div>
+                        </p>
+                    </div>
+                    <img onClick={handleClick} className='w-7 h-7 self-center mx-auto cursor-pointer hover:scale-125' src={right} alt="" />
+                </div>
+            }
         </div>
     )
 }
